4f66d20cc627cc9e2c089ecbf8acf40f03926dd1
zhoupaopao <1312667058@qq.com>
2018-06-04 10:26:21 +0800

    6.1修改

--- app/src/main/java/com/ledt/Activity/RefreshSJBBActivity.java
+++ app/src/main/java/com/ledt/Activity/RefreshSJBBActivity.java
@@ -1,11 +1,34 @@
 package com.ledt.Activity;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.ledt.R;
 
 /**
  * Created by Lenovo on 2018/6/1.
  */
 
 public class RefreshSJBBActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_refresh_sjbb);
+        initView();
+        initData();
+        initListener();
+    }
 
+    private void initView() {
+
+    }
+
+    private void initData() {
+
+    }
+
+    private void initListener() {
+
+    }
 }
