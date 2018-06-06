e571abdb2db63f47d689339a27dffd74fff3ab8e
zhoupaopao <1312667058@qq.com>
2018-06-06 08:45:45 +0800

    6.56修改

--- app/src/main/java/com/ledt/Activity/LeftBackActivity.java
+++ app/src/main/java/com/ledt/Activity/LeftBackActivity.java
@@ -17,6 +17,7 @@ public class LeftBackActivity extends SwipeBackActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //这个只是对标题进行修改的
 setContentView(R.layout.activity_left_back);
         ImmersionBar.with(this)
                 .titleBar(R.id.toolbar)
